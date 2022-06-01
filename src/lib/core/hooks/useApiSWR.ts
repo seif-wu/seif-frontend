eea5c5912559c2e803dce7bf72a6b4450587e314
Seif Wu <seif.wuxf@gmail.com>
2022-06-01 15:31:13 +0800

    feat: useApiSWR 添加 options

--- src/lib/core/hooks/useApiSWR.ts
+++ src/lib/core/hooks/useApiSWR.ts
@@ -1,16 +1,28 @@
-import useSWR from 'swr';
+import useSWR, { BareFetcher } from 'swr';
 import type { Key } from 'swr';
 import api from '../api';
+import { PublicConfiguration } from 'swr/dist/types';
 
-function useApiSWR(key: Key) {
-  const { data, error, ...other } = useSWR(key, api.get);
+function useApiSWR(
+  key: Key,
+  options?:
+    | Partial<
+        PublicConfiguration<
+          { response: any; status: number },
+          any,
+          BareFetcher<{ response: any; status: number }>
+        >
+      >
+    | undefined
+) {
+  const { data, error, ...other } = useSWR(key, api.get, options);
 
   return {
     data: data?.response?.data,
     error: error?.error,
     status: data?.status || error?.status,
     isLoading: !data,
-    ...other
+    ...other,
   };
 }
 
